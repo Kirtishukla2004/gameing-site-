31a258e37850e095c82f8661884d6b3a6b3aee37
kirti <shuklakirti2004@gmail.com>
2023-12-02 01:58:03 +0530

    changes

--- gaming-world/src/Component/Header.js
+++ gaming-world/src/Component/Header.js
@@ -3,15 +3,22 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <header className="header">
-                <h1>Header Title</h1>
-                <nav>
-                    <ul>
-                        <li className='text-xl'><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </nav>
+            <header className="bg-gray-800 p-4 flex items-center justify-between">
+                <div className="flex items-center">
+                   
+                    <div className="flex items-center">
+                        <img src="./public/logo.jpg" alt="logo"/>
+                        <span className="text-white text-lg font-semibold">Home</span>
+                    </div>
+                </div>
+                <div className="flex items-center">
+                   
+                    <span className="text-white mx-4 cursor-pointer">Play</span>
+                   
+                    <span className="text-white mx-4 cursor-pointer">Points</span>
+                    
+                    <span className="text-white mx-4 cursor-pointer">Profile</span>
+                </div>
             </header>
         );
     }
