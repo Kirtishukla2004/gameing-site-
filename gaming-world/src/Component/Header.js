b926866a3a6e570efeeeae6abb8d12f67e3e2cc5
kirti <shuklakirti2004@gmail.com>
2023-12-02 15:09:59 +0530

    changes

--- gaming-world/src/Component/Header.js
+++ gaming-world/src/Component/Header.js
@@ -3,21 +3,26 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <header className="bg-gray-800 p-4 flex items-center justify-between">
+            <header className=" p-4 flex items-center justify-between ">
+                <div className='bg-[radial-gradient(circle,rgba(24,0,255,0.07),rgba(24,0,255,0.07)_38%,#000),url("https://assets-global.website-files.com/64fad25f8884cb2d680a1052/651438da4d065110b561b1b2\_hero-bg-blocks.svg")] bg-[0_0,50%] bg-[auto,cover] justify-center items-center flex pt-24 pb-8
+'>
+                    
+                </div>
                 <div className="flex items-center">
-                   
+                    {/* Logo and Home */}
                     <div className="flex items-center">
-                        <img src="./public/logo.jpg" alt="logo"/>
-                        <span className="text-white text-lg font-semibold">Home</span>
+                        <img src="./logo.jpg" alt="logo" className="h-8 mr-4" />
+                        
                     </div>
                 </div>
                 <div className="flex items-center">
-                   
-                    <span className="text-white mx-4 cursor-pointer">Play</span>
-                   
-                    <span className="text-white mx-4 cursor-pointer">Points</span>
-                    
-                    <span className="text-white mx-4 cursor-pointer">Profile</span>
+                <span className=" mx-4 cursor-pointer">Home</span>
+                    {/* Play */}
+                    <span className=" mx-4 cursor-pointer">Play</span>
+                    {/* Points */}
+                    <span className=" mx-4 cursor-pointer">Points</span>
+                    {/* Profile */}
+                    <span className=" mx-4 cursor-pointer">Profile</span>
                 </div>
             </header>
         );
