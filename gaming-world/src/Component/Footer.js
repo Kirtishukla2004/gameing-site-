2d44d130b3e024b63afe1e3b141334db2b846433
kirti shukla <104082719+Kirtishukla2004@users.noreply.github.com>
2023-12-13 00:54:20 +0530

    Revert "footer✔"

---
+++ gaming-world/src/Component/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class Footer extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <footer class="bg-gray-800 text-white text-center py-4 footer">
+          <p>&copy; 2023 Sample Footer. All rights reserved.</p>
+        </footer>
+       
+      </>
+    );
+  }
+}
+
+export default Footer;
