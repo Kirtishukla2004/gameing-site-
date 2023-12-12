f110fdbfac59bc8fc42252f0314752f4d28aed05
Tanu Kaushik <developertanukaushik@gmail.com>
2023-12-12 21:49:45 +0530

    footer✔

--- gaming-world/src/Footer.js
+++ gaming-world/src/Footer.js
@@ -4,6 +4,7 @@ class Footer extends Component {
     render() { 
         return (
             <>
+            <div style={{ padding: "200px" }}></div>
             <footer class="bg-gray-800 text-white text-center py-4">
     
     <p>&copy; 2023 Sample Footer. All rights reserved.</p>
